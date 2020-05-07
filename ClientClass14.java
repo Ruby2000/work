c03fc2f6f0436dd992e5eb91ac84075a5d9579f4
Ruby2000 <42425684+Ruby2000@users.noreply.github.com>
2020-05-07 13:24:21 +0800

    Add files via upload

---
+++ ClientClass14.java
@@ -0,0 +1,13 @@
+package proxy;
+
+import proxy.ClocalPicShow;
+
+public class ClientClass14 {    //静态代理
+	public static void main(String args[]) {
+	ClocalPicShow cls =new ClocalPicShow();
+	cls.ShowPic("photo");
+//	IShowPic iPic =new IShowPic();
+//	IShowPic borax =(IShowPic) new ClocalPicShow();
+//	proxy.ShowPic("AAA");
+}
+}
